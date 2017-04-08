bb54f4055ca12c7e7f19c07e2a102a62202ec60e
yb-bs <ybbs.dev@gmail.com>
2017-04-08 18:02:46 +0530

    Including Procfile for Heroku deployment

--- app.js
+++ app.js
@@ -1,43 +1,12 @@
 var exp = require("express");
 var app = exp();
+var handlers = require("./routes/routes.js");
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-	res.render('home.ejs', {
-		title:'I love my city',
-		headline:'Every city has its own personality'
-		
-	});
-});
+app.get('/', handlers.homeFn);
 
-app.get('/:city', function(req, res) {
-	var cityname = req.params.city;
-	var titleValue;
-	var headlineValue;
-	
-	if(cityname === 'newyork'){
-		titleValue="New York";
-		headlineValue="Business capital of the world"
-	}
-	else if(cityname === 'london'){
-		titleValue="London";
-		headlineValue="City of the Thames"
-	}
-	else if(cityname === 'newdelhi'){
-		titleValue="New Delhi";
-		headlineValue="Capital city of India"
-	}
-	else if(cityname === 'paris'){
-		titleValue="Paris";
-		headlineValue="Fashion capital of the world"
-	}
-	
-	res.render('city.ejs', {
-		title:titleValue,
-		headline:headlineValue
-	});
-});
+app.get('/:city', handlers.cityFn);
 
 var port = process.env.PORT || 4000;
 app.listen(port, function() {
